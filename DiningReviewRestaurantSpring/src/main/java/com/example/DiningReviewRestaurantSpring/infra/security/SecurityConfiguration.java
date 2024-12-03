16f5aea8b31353c20dec46e3f1d39854a54dc7b0
Paulo <paulorolinski.ifc@gmail.com>
2024-12-03 17:43:37 -0300

    feat: JUnit test

--- DiningReviewRestaurantSpring/src/main/java/com/example/DiningReviewRestaurantSpring/infra/security/SecurityConfiguration.java
+++ DiningReviewRestaurantSpring/src/main/java/com/example/DiningReviewRestaurantSpring/infra/security/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.example.DiningReviewRestaurantSpring.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- DiningReviewRestaurantSpring/src/test/java/com/example/DiningReviewRestaurantSpring/repositories/UserRepositoryTest.java
+++ DiningReviewRestaurantSpring/src/test/java/com/example/DiningReviewRestaurantSpring/repositories/UserRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.example.DiningReviewRestaurantSpring.repositories;
 
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 class UserRepositoryTest {
+    @After()
+    void findByLogin() {
 
+    }
 }
